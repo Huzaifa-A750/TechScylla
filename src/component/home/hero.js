927eae3abb7cbc57b3dc0811b199de25a18571dd
Huzaifa-A750 <huzaifaafzal750@gmail.com>
2024-01-26 01:04:15 +0500

    hero updated

--- src/component/home/hero.js
+++ src/component/home/hero.js
@@ -3,8 +3,8 @@ import Heroimg from "../../assets/images/hero.png";
 
 export default function Hero() {
   return (
-    <div className="py-20 px-10">
-      <div className="text-[#1B1B1B] font-[Avenir-Next-Regular] font-semibold grid justify-center text-[45px] leading-[54px]">
+    <div className="py-5 sm:py-20 px-10">
+      <div className="text-[#1B1B1B] font-[Avenir-Next-Regular] font-semibold grid justify-center text-[28px] sm:text-[45px] leading-[40px] sm:leading-[54px]">
         <h1 className="w-auto md:w-[733px] text-center">
           Navigating the Digital Depths with Innovation and{" "}
           <span className="text-[#2678F2]">
@@ -15,7 +15,7 @@ export default function Hero() {
           </span>
         </h1>
       </div>
-      <div className="grid justify-center py-20">
+      <div className="grid justify-center py-10 sm:py-20">
         <img src={Heroimg} alt="Hero" />
       </div>
     </div>
